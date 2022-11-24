7545be9ccea36d61a3c2385b09212ea2c8f9407e
Yasin Şükrü TAN <yasinsukrutan@gmail.com>
2022-11-24 12:34:06 +0300

    Initial Commit

---
+++ GridCalendar/GridCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridCalendarApp.swift
+//  GridCalendar
+//
+//  Created by Yasin Şükrü Tan on 24.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct GridCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
