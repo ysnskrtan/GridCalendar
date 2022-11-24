e8e875f3e918f1ca9f628deac276bbc838f0f5c8
Yasin Şükrü TAN <yasinsukrutan@gmail.com>
2022-11-24 13:16:00 +0300

    New files are created: day and month

---
+++ GridCalendar/Model/Day.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  GridCalendar
+//
+//  Created by Yasin Şükrü Tan on 24.11.2022.
+//
+
+import Foundation
+
+
+struct Day: Identifiable {
+    let id = UUID()
+    let value: Int
+}
---
+++ GridCalendar/Model/Month.swift
@@ -0,0 +1,8 @@
+//
+//  Month.swift
+//  GridCalendar
+//
+//  Created by Yasin Şükrü Tan on 24.11.2022.
+//
+
+import Foundation
